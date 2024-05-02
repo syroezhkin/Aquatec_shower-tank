08f335fc8b8ae4fa11583aa0b50dac32cfbb9ff1
Vladimir Syroezhkin <vladimir@syroezhkin.net>
2024-05-02 18:50:34 +0800

    Модель ёмкости

--- definitions/stdl.lua
+++ definitions/stdl.lua
@@ -1,8 +1,16 @@
 ---@meta
 
 ---@alias Solid table
-
 Solid = {}
+
+---@alias Point3D table
+Point3D = {}
+
+---@alias Vector3D table
+Vector3D = {}
+
+---@alias Axis3D table
+Axis3D = {}
 
 function Solid:Shift(x, y, z) end
 
@@ -12,17 +20,30 @@ function SetSymbolicGeometry(geometry) end
 
 function SetSymbolGeometry(geometry) end
 
+---@alias Placement3D Placement3D
+function Placement3D(point, z_axis_direction, x_axis_direction) end
+
 ---@return Solid
 function CreateBlock(xSize, ySize, zSize) end
 
+---@param width number
+---@param depth number
+---@param height number
+---@param placement? Placement3D
 ---@return Solid
-function CreateRectangularPyramid(width, depth, height) end
+function CreateRectangularPyramid(width, depth, height, placement) end
 
 ---@return Solid
 function CreateRightCircularCylinder(radius, height) end
 
 ---@return Solid
 function Unite(solids) end
+
+---@return Solid
+function Subtract(solid1, solid2) end
+
+---@return Solid
+function Intersect(solid1, solid2) end
 
 Style = {}
 
--- main.lua
+++ main.lua
@@ -1,15 +1,32 @@
 local parameters = Style.GetParameterValues()
 local dimensions = parameters.Dimensions
 
+function MakeBottom(M, Width)
+	return Subtract(
+		CreateRectangularPyramid(Width - 3 * M, Width - 3 * M, -1.25 * M):Shift(0, 0, -M),
+		CreateBlock(Width, Width, 2 * M):Shift(0, 0, -4 * M)
+	)
+end
+
 function MakeTank(M, NeckDiameter, Width, Height)
-	local h = Height - 5 * M
+	local h = Height - 4 * M
+	local baseAxes = Axis3D(Point3D(0, 0, 0), Vector3D(0, 0, 1))
+	local bottomPiramidPlacement = Placement3D(Point3D(0, 0, -M), Vector3D(0, 0, -1), Vector3D(1, 0, 0))
+	local rib = CreateBlock(2 * M, Width / 2 - 4 * M, M):Shift(0, Width / 4, h)
 	return Unite({
-		CreateRightCircularCylinder(NeckDiameter / 2, 3 * M):Shift(0, 0, h),
-		CreateBlock(NeckDiameter + M, NeckDiameter + M, 2 * M):Shift(0, 0, h),
-		CreateRectangularPyramid(Width - 2 * M, Width - 2 * M, 2 * M):Shift(0, 0, h),
+		rib,
+		rib:Clone():Rotate(baseAxes, math.pi / 2),
+		rib:Clone():Rotate(baseAxes, -math.pi / 2),
+		CreateBlock(2 * M, Width / 2, M):Shift(0, -Width / 4, h),
+		CreateRightCircularCylinder(NeckDiameter / 2, 1.5 * M):Shift(0, 0, h),
+		Intersect(
+			CreateRectangularPyramid(Width - 2 * M, Width - 2 * M, 2 * M):Shift(0, 0, h),
+			CreateBlock(Width, Width, M):Shift(0, 0, h)
+		),
 		CreateBlock(Width, Width, h),
 		CreateBlock(Width - M, Width - M, M):Shift(0, 0, -M),
-		CreateRectangularPyramid(Width - 3 * M, Width - 3 * M, -M):Shift(0, 0, -M),
+		CreateRectangularPyramid(Width - 3 * M, Width - 3 * M, M, bottomPiramidPlacement),
+		CreateBlock(M, Width / 2, 1.5 * M):Shift(0, Width / 4 - M / 2, -2.5 * M),
 	})
 end
 
